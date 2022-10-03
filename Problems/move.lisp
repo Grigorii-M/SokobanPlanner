937fc809de83582cb777a978cf3fab50ec8749ec
Grigorii Matiukhin <grigory36m@gmail.com>
2022-10-03 23:32:52 +0300

    Added an ability to move around a tilemap and to push boxes along

---
+++ Problems/move.lisp
@@ -0,0 +1,94 @@
+(define (problem Move)
+    (:domain SokobanOnSteroids)
+    (:objects
+      ;;; The map
+      t_0_0 - tile
+      t_1_0 - tile
+      t_2_0 - tile
+      t_3_0 - tile
+      t_4_0 - tile
+      t_0_1 - tile
+      t_1_1 - tile
+      t_2_1 - tile
+      t_3_1 - tile
+      t_4_1 - tile
+      t_0_2 - tile
+      t_1_2 - tile
+      t_2_2 - tile
+      t_3_2 - tile
+      t_4_2 - tile
+      t_0_3 - tile
+      t_1_3 - tile
+      t_2_3 - tile
+      t_3_3 - tile
+      t_4_3 - tile
+      t_0_4 - tile
+      t_1_4 - tile
+      t_2_4 - tile
+      t_3_4 - tile
+      t_4_4 - tile
+
+      ;;; Other
+      sokoban - agent
+    )
+ 
+    (:init
+      ;;; Set up the map
+      ;; connect rows
+      (adjacent t_0_0 t_1_0)
+      (adjacent t_1_0 t_2_0)
+      (adjacent t_2_0 t_3_0)
+      (adjacent t_3_0 t_4_0)
+
+      (adjacent t_0_1 t_1_1)
+      (adjacent t_1_1 t_2_1)
+      (adjacent t_2_1 t_3_1)
+      (adjacent t_3_1 t_4_1)
+
+      (adjacent t_0_2 t_1_2)
+      (adjacent t_1_2 t_2_2)
+      (adjacent t_2_2 t_3_2)
+      (adjacent t_3_2 t_4_2)
+
+      (adjacent t_0_3 t_1_3)
+      (adjacent t_1_3 t_2_3)
+      (adjacent t_2_3 t_3_3)
+      (adjacent t_3_3 t_4_3)
+
+      (adjacent t_0_4 t_1_4)
+      (adjacent t_1_4 t_2_4)
+      (adjacent t_2_4 t_3_4)
+      (adjacent t_3_4 t_4_4)
+
+      ;; connect columns
+      (adjacent t_0_0 t_0_1)
+      (adjacent t_1_0 t_1_1)
+      (adjacent t_2_0 t_2_1)
+      (adjacent t_3_0 t_3_1)
+      (adjacent t_4_0 t_4_1)
+
+      (adjacent t_0_1 t_0_2)
+      (adjacent t_1_1 t_1_2)
+      (adjacent t_2_1 t_2_2)
+      (adjacent t_3_1 t_3_2)
+      (adjacent t_4_1 t_4_2)
+
+      (adjacent t_0_2 t_0_3)
+      (adjacent t_1_2 t_1_3)
+      (adjacent t_2_2 t_2_3)
+      (adjacent t_3_2 t_3_3)
+      (adjacent t_4_2 t_4_3)
+
+      (adjacent t_0_3 t_0_4)
+      (adjacent t_1_3 t_1_4)
+      (adjacent t_2_3 t_2_4)
+      (adjacent t_3_3 t_3_4)
+      (adjacent t_4_3 t_4_4)
+
+      ;;; Set up the objects
+      (at sokoban t_0_0)
+    )
+ 
+    (:goal
+      (at sokoban t_4_4)
+    )
---
+++ domain.lisp
@@ -0,0 +1,55 @@
+(define (domain SokobanOnSteroids)
+  (:requirements
+    :strips
+    :negative-preconditions
+  )
+
+  (:types
+    object location agent - type
+    box - object
+    tile - location
+  )
+
+  (:constants
+
+  )
+
+  (:predicates
+    (at ?x - agent ?p - tile) ;; whether sokoban is at position x
+    (object_at ?x - object ?p - tile)
+    (adjacent ?x - tile ?y - tile) ;; whether x tile is adjacent to y tile
+    ;; whether movements x -> y and y -> z are in the same direction
+    (same_direction ?x - tile ?y - tile ?x - tile)
+  )
+
+  (:functions
+
+  )
+
+  (:action move
+    :parameters(?x - tile ?y - tile)
+    :precondition(and (at sokoban ?x) (adjacent ?x ?y))
+    :effect(and (not(at sokoban ?x)) (at sokoban ?y))
+  )
+
+  (:action push
+    :parameters(
+      ?px - tile ;; sokoban's current position
+      ?py - tile ;; sokoban's next position and the box' current position
+      ?ox - tile ;; box' new position
+      ?obj - box
+    )
+    :precondition(and
+      (at sokoban ?px)
+      (adjacent ?px ?py)
+      (object_at ?obj ?py)
+      (same_direction ?px ?py ?ox)
+    )
+    :effect(and
+      (not(at sokoban ?px))
+      (at sokoban ?py)
+      (not (object_at box1 ?py))
+      (object_at ?obj ?ox)
+    )
+  )
+)
