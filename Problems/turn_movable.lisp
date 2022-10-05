69b8ffd6c1158c14c1b4c2a9504141d2d5f11b9d
Grigorii Matiukhin <grigory36m@gmail.com>
2022-10-05 16:21:05 +0300

    Added immovable boxes, split a bunch of actions

---
+++ Problems/turn_movable.lisp
@@ -0,0 +1,63 @@
+;;;; Here we test, whether Sokoban can turn an immovable box into a movable one in order to push it
+
+(define (problem turn_movable)
+  (:domain SokobanOnSteroids)
+  (:objects
+    ;;; The map
+    ;; _ _ _
+    ;; b B S
+    ;; *
+
+    t_0_0 - tile
+    t_1_0 - tile
+    t_2_0 - tile
+    
+    t_0_1 - tile
+    t_1_1 - tile
+    t_2_1 - tile
+
+    t_0_2 - tile
+
+    ;;; Other
+    box1 - box
+    bomb1 - bomb
+  )
+
+  (:init
+    ;;; Set up the map
+    ;; Make things adjacent
+    ; Horizontal
+    (adjacent t_0_0 t_1_0)(adjacent t_1_0 t_0_0)
+    (adjacent t_1_0 t_2_0)(adjacent t_2_0 t_1_0)
+
+    (adjacent t_0_1 t_1_1)(adjacent t_1_1 t_0_1)
+    (adjacent t_1_1 t_2_1)(adjacent t_2_1 t_1_1)
+
+    ; Vertical
+    (adjacent t_0_0 t_0_1)(adjacent t_0_1 t_0_0)
+    (adjacent t_1_0 t_1_1)(adjacent t_1_1 t_1_0)
+    (adjacent t_2_0 t_2_1)(adjacent t_2_1 t_2_0)
+
+    (adjacent t_0_1 t_0_2)(adjacent t_0_2 t_0_1)
+
+    ;; Configure directions
+    (same_direction t_0_0 t_1_0 t_2_0)(same_direction t_2_0 t_1_0 t_0_0)
+    (same_direction t_0_1 t_1_1 t_2_1)(same_direction t_2_1 t_1_1 t_0_1)
+
+    (same_direction t_0_0 t_0_1 t_0_2)(same_direction t_0_2 t_0_1 t_0_0)
+
+    ;;; Set up the objects
+    (sokoban_at t_2_1)
+
+    (entity_at box1 t_0_1)
+    (box_immovable box1)
+    (inaccessible t_0_1)
+
+    (entity_at bomb1 t_1_1)
+    (inaccessible t_1_1)
+  )
+
+  (:goal
+    (entity_at box1 t_0_2)
+  )
+)
--- domain.lisp
+++ domain.lisp
@@ -5,16 +5,13 @@
   )
 
   (:types
-    object location - object
+    entity location - object
 
     tile - location
     
-    movable - entity
-    immovable - entity
-
-    box - movable
-    bomb - movable
-    wall - immovable
+    box - entity
+    bomb - entity
+    wall - entity
   )
 
   (:predicates
@@ -24,6 +21,7 @@
     ;; whether movements x -> y and y -> z are in the same direction
     (same_direction ?x - tile ?y - tile ?x - tile)
     (inaccessible ?x - tile)
+    (box_immovable ?b - box)
   )
 
   (:action move
@@ -32,12 +30,64 @@
     :effect(and (not (sokoban_at ?x)) (sokoban_at ?y))
   )
 
-  (:action push
+  (:action push_box
     :parameters(
       ?x - tile ;; sokoban's current position
       ?y - tile ;; sokoban's next position and the box' current position
       ?z - tile ;; box' new position
-      ?b - movable
+      ?b - box
+    )
+    :precondition(and
+      (sokoban_at ?x)
+      (or (adjacent ?x ?y) (adjacent ?y ?x))
+      (or (adjacent ?x ?y) (adjacent ?y ?x))
+      (not (inaccessible ?z))
+      (entity_at ?b ?y)
+      (same_direction ?x ?y ?z)
+      (not (box_immovable ?b))
+    )
+    :effect(and
+      (not (sokoban_at ?x))
+      (sokoban_at ?y)
+      (not (entity_at ?b ?y))
+      (entity_at ?b ?z)
+      (not (inaccessible ?y))
+      (inaccessible ?z)
+    )
+  )
+
+  (:action pull_box
+    :parameters(
+      ?z - tile ;; box' current position
+      ?y - tile ;; sokoban's current position and the box' next position
+      ?x - tile ;; sokoban's next position
+      ?b - box
+    )
+    :precondition(and
+      (sokoban_at ?y)
+      (or (adjacent ?x ?y) (adjacent ?y ?x))
+      (or (adjacent ?x ?y) (adjacent ?y ?x))
+      (not (inaccessible ?x))
+      (entity_at ?b ?z)
+      (same_direction ?x ?y ?z)
+      (not (box_immovable ?b))
+    )
+    :effect(and
+      (not (sokoban_at ?y))
+      (sokoban_at ?x)
+      (not (entity_at ?b ?z))
+      (entity_at ?b ?y)
+      (not (inaccessible ?z))
+      (inaccessible ?y)
+    )
+  )
+  
+  (:action push_bomb
+    :parameters(
+      ?x - tile ;; sokoban's current position
+      ?y - tile ;; sokoban's next position and the box' current position
+      ?z - tile ;; box' new position
+      ?b - bomb
     )
     :precondition(and
       (sokoban_at ?x)
@@ -57,12 +107,12 @@
     )
   )
 
-  (:action pull
+  (:action pull_bomb
     :parameters(
       ?z - tile ;; box' current position
       ?y - tile ;; sokoban's current position and the box' next position
       ?x - tile ;; sokoban's next position
-      ?b - movable
+      ?b - bomb
     )
     :precondition(and
       (sokoban_at ?y)
@@ -82,14 +132,14 @@
     )
   )
 
-  (:action blow_stuff_up
+  (:action destroy_wall
     :parameters(
       ?x - tile ;; sokoban's current position
       ?y - tile ;; bomb's position
       ?b - bomb
 
       ?t - tile
-      ?e - entity
+      ?e - wall
     )
     :precondition(and
       (sokoban_at ?x)
@@ -103,6 +153,55 @@
       (not (inaccessible ?y))
       (not (entity_at ?e ?t))
       (not (inaccessible ?t))
+    )
+  )
+
+  (:action destroy_box
+    :parameters(
+      ?x - tile ;; sokoban's current position
+      ?y - tile ;; bomb's position
+      ?b - bomb
+
+      ?t - tile
+      ?e - box
+    )
+    :precondition(and
+      (sokoban_at ?x)
+      (entity_at ?b ?y)
+      (entity_at ?e ?t)
+      (adjacent ?x ?y)
+      (same_direction ?x ?y ?t)
+      (not (box_immovable ?e))
+    )
+    :effect(and
+      (not (entity_at ?b ?y))
+      (not (inaccessible ?y))
+      (not (entity_at ?e ?t))
+      (not (inaccessible ?t))
+    )
+  )
+  
+  (:action make_box_movable
+    :parameters(
+      ?x - tile ;; sokoban's current position
+      ?y - tile ;; bomb's position
+      ?b - bomb
+
+      ?t - tile
+      ?e - box
+    )
+    :precondition(and
+      (sokoban_at ?x)
+      (entity_at ?b ?y)
+      (entity_at ?e ?t)
+      (adjacent ?x ?y)
+      (same_direction ?x ?y ?t)
+      (box_immovable ?e)
+    )
+    :effect(and
+      (not (entity_at ?b ?y))
+      (not (inaccessible ?y))
+      (not (box_immovable ?e))
     )
   )
 )
