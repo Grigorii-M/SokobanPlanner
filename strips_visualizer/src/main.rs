6e9ab15b98fb951e48a59b0ea0fb1b8cd23f2006
Grigorii Matiukhin <grigory36m@gmail.com>
2022-10-05 18:36:13 +0300

    Added map files

--- strips_visualizer/src/main.rs
+++ strips_visualizer/src/main.rs
@@ -1,32 +1,50 @@
-use game_engine::*;
+use std::fs::File;
+use std::io::prelude::*;
+use clap::Parser;
 
-struct Application {}
+enum Tile {
+    Sokoban,
+    MovableBox,
+    ImmovableBox,
+    Bomb,
+    Wall,
+    Floor,
+}
 
-impl GameObject for Application {
-    fn update(
-        &mut self,
-        game_state: &mut GameState,
-        graphics_engine: &mut gfx::GraphicsEngine,
-        input_handler: &mut input::InputHandler,
-    ) {
-        if input_handler.is_key_down(&input::VirtualKeyCode::Escape) {
-            game_state.exit();
+struct Map {
+    tiles: Vec<Tile>,
+}
+
+impl Map {
+    fn from_file(file: &File) -> Self {
+        Self {
+            tiles: vec![],
         }
     }
 }
 
-fn main() {
-    let mut game = Game::new(
-        "STRIPS Visualizer",
-        WindowSettings {
-            logical_width: 100,
-            logical_height: 100,
-            resize_mode: ResizeMode::KeepAspectRatio,
-        }
-    );
-    
-    let app = Application {};
-    game.add_game_object(app);
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    map_file: String,
 
-    game.run();
+    #[arg(short, long)]
+    plan_file: String,
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+
+    let mut map_file = File::open(args.map_file)?;
+    let mut map_str = String::new();
+    map_file.read_to_string(&mut map_str)?;
+    println!("{map_str}");
+
+    let mut plan_file = File::open(args.plan_file)?;
+    let mut plan_str = String::new();
+    plan_file.read_to_string(&mut plan_str)?;
+    println!("{plan_str}");
+    
+    Ok(())
 }
